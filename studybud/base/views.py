28f0da3bba878928103b1ea8ecc4593ed79ee28a
= <=>
2024-07-28 23:49:58 +0200

    Static files added, minor changes.

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -107,7 +107,9 @@ def create_room(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
 
     context = {'form': form}
